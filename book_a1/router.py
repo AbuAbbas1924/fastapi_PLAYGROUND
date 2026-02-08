d48357179bbd7d32f305ee608b633d0ffe2559a3
shuhib <shuhib_s@live.de>
2026-02-08 20:55:02 +0100

    alembic + API

--- book_a1/router.py
+++ book_a1/router.py
@@ -1,9 +1,9 @@
 import uuid
-from datetime import datetime, timezone
+from datetime import datetime
 from typing import Annotated
 
 import sqlalchemy.dialects.postgresql as pg
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from sqlmodel import Column, Field, SQLModel, select
 from sqlmodel.ext.asyncio.session import AsyncSession
@@ -24,21 +24,28 @@ class Book(SQLModel, table=True):
     published_data: str
     page_count: int
     language: str
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
-    update_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    created_at: datetime = Field(default_factory=datetime.now)
+    update_at: datetime = Field(default_factory=datetime.now)
 
     def __repr__(self) -> str:
         return f"book-(uid={self.uid}, title={self.title}, author={self.author})"
 
 
 class BookCreateModel(BaseModel):
-    title: str | None = None
-    author: str | None = None
-    publisher: str | None = None
-    published_date: str | None = None
-    page_count: str | None = None
-    language: str | None = None
+    title: str
+    author: str
+    publisher: str
+    published_data: str
+    page_count: int
+    language: str
 
+class BookUpdateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language: str
 
 # * 2. services
 
@@ -62,18 +69,53 @@ class BookService:
         result = await self.session.execute(select(Book).order_by(Book.created_at))
         return result.scalars().all()
 
+    async def get_book(self, book_uid: str):
+        result = await self.session.execute(select(Book).where(Book.uid == book_uid))
+        result = result.scalar_one_or_none()
+
+    async def update_book(self, book_uid: str, book_data: BookUpdateModel):
+        book_to_update = await self.get_book(book_uid)
+        if not book_to_update:
+            raise HTTPException(status_code=303, detail="book not found")
+        data_dict = book_data.model_dump()
+        for key, value in data_dict.items():
+            setattr(book_to_update, key, value)
+
+        return await self.save(book_to_update)
+
+    async def delete_book(self, book_uid: str):
+        book_to_delete = await self.get_book(book_uid)
+        if not book_to_delete:
+            raise HTTPException(status_code=404, detail="book not found")
+        await self.session.delete(book_to_delete)
+        await self.session.commit()
+        return book_to_delete
 
 router = APIRouter(prefix="/book_a1", tags=["book_a1"])
 session_dep = Annotated[AsyncSession, Depends(db)]
 
+@router.post("/", status_code=201)
+async def create_book(book_data: BookCreateModel, session: session_dep):
+    service = BookService(session)
+    return await service.create_book(book_data)
 
-@router.get("/")
+@router.get("/", status_code=200)
 async def get_books(session: session_dep):
     service = BookService(session=session)
     return await service.get_books()
 
+@router.get("/{book_uid}")
+async def get_book(book_uid: str, session: session_dep):
+    service = BookService(session=session)
+    return await service.get_book(book_uid)
 
-@router.post("/")
-async def create_book(book_data: BookCreateModel, session: session_dep):
-    service = BookService(session)
-    return await service.create_book(book_data)
+
+@router.put("/{book_uid}")
+async def update_book(book_uid: str, book_data: BookUpdateModel, session: session_dep):
+    service = BookService(session=session)
+    return await service.update_book(book_uid, book_data)
+
+@router.delete("/{book_uid}")
+async def delete_book(book_uid: str, session: session_dep):
+    service = BookService(session=session)
+    return service.delete_book(book_uid)
--- migrations/env.py
+++ migrations/env.py
@@ -5,15 +5,21 @@ from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlmodel import SQLModel
 
+from book_a1.postgres import postgres_settings
+from book_a1.router import Book
+
+database_url = postgres_settings.DATABASE_URL
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-
+config.set_main_option("sqlalchemy.url", database_url)
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+target_metadata = SQLModel.metadata
 
 # add your model's MetaData object here
 # for 'autogenerate' support
